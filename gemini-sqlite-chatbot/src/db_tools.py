e3e98168dc52871e8360c4d8fd2fa582d86dc3b0
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-04-28 09:12:54 +0400

    modified:   gemini-sqlite-chatbot/src/db_tools.py

--- gemini-sqlite-chatbot/src/db_tools.py
+++ gemini-sqlite-chatbot/src/db_tools.py
@@ -5,29 +5,31 @@ db_file = os.path.join(os.path.dirname(__file__), '..', 'database', 'sample.db')
 db_conn = sqlite3.connect(db_file)
 
 def list_tables() -> list[str]:
-    """Retrieve all table names."""
+    """Retrieve the names of all tables in the database."""
+    print(' - DB CALL: list_tables()')
+    
     cursor = db_conn.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    return [t[0] for t in cursor.fetchall()]
+    tables = cursor.fetchall()
+    return [t[0] for t in tables]
 
 def describe_table(table_name: str) -> list[tuple[str, str]]:
-    """Get table schema."""
+    """Look up the table schema.
+    
+    Returns:
+      List of columns, where each entry is a tuple of (column, type).
+    """
+    print(f' - DB CALL: describe_table({table_name})')
+    
     cursor = db_conn.cursor()
     cursor.execute(f"PRAGMA table_info({table_name});")
-    return [(col[1], col[2]) for col in cursor.fetchall()]
+    schema = cursor.fetchall()
+    return [(col[1], col[2]) for col in schema]
 
 def execute_query(sql: str) -> list[list[str]]:
-    """Execute SQL query."""
+    """Execute an SQL statement, returning the results."""
+    print(f' - DB CALL: execute_query({sql})')
+    
     cursor = db_conn.cursor()
     cursor.execute(sql)
     return cursor.fetchall()
-
-def plot_orders_per_stuff():
-    """Plot the number of orders handled by each staff member."""
-    sql= """
-    SELECT s.name, COUNT(o.order_id)
-    from orders o
-    JOIN staff s ON o.staff_id = s.staff_id
-    GROUP BY s.name
-    ORDER BY COUNT(o.order_id) DESC
-    """
